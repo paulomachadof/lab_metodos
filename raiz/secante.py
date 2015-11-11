afada137174c09905429f3670b467718c946b42d
paullomachado <paulomachado@outlook.com>
2015-11-11 14:23:31 -0300

    Implementando o método da Secante

---
+++ raiz/secante.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+'''
+Created on Nov , 11 2015
+
+@author: paulomachado
+'''
+from math import *
+
+def secante(f, df, x0, x1, epsilon, maxIter = 50):
+    """Executa o método da Secante para achar o zero de f 
+       com precisão epsilon. O método executa no máximo maxIter
+       Retorna uma tupla (houveErro, raiz), onde houveErro é booleano.
+    """
+   # Testando se X0 já é raiz    
+    if abs(f(x0)) <= epsilon:
+        return (False, x0)
+
+   # Testando se X1 já é raiz
+    if abs(f(x1)) <= epsilon:
+        return (False, x1)
+    
+   # Mostrando Cabeçalho
+    print("K\t     X\t           F(x)            F'(x)")
+
+    for k in range(1, maxIter+1):
+        x2 = x1 - f(x1)/(f(x1) - f(x0)) * (x1 - x0)
+        
+	# Mostra valores na tela
+        print("%d\t %e\t %e\t %e"%(k,x1,f(x1),df(x1)))
+    
+        # Teste do critério de parada módulo da função
+        if abs(f(x0)) <= epsilon:
+            return (False, x0)
+        if abs(f(x1)) <= epsilon:
+            return (False, x1)
+        x0 = x1
+        x1 = x2
+
+    # Se chegar aqui é porque o número máximo de iterações foi atingido
+    print("O Número máximo de interações foi atingido")
+    return(True, x1)
+    
+if __name__ == "__main__":
+                                       
+    def f(x):
+        return x**3-9*x+3
+    def df(x):
+        return 3*x**2-9
+    x0 = 0
+    x1 = 1
+    epsilon = 0.0001
+    maxIter = 20
+    print("Método da Secante")
+    (houveErro, raiz) = secante(f,df,x1,x0,epsilon)
+    if houveErro:
+        print("O Método de Newton-Raphson a retornou um erro.")
+    if raiz is not None:	
+        print("Raiz encontrada: %e"%raiz)
