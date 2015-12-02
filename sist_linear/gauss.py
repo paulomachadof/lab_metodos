776106603bc2faa98d51bb9a260a8c05a9ce4f3e
Paulo Machado <paulomachado@outlook.com>
2015-12-02 18:24:02 -0300

    Método de Gauss

--- sist_linear/gauss.py
+++ sist_linear/gauss.py
@@ -23,7 +23,7 @@ def gauss(n, A, b):
     # escreva o código aqui
     for k in range(0,n-1):
         for i in range(k+1,n):
-            m = A[i][k]/A[k][k]
+            m = -A[i][k]/A[k][k]
             A[i][k] = 0
             for j in range(k+1,n):
                 A[i][j] = m * A[k][j] + A[i][j]
