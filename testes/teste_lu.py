19caf9e43645036ce6b9e71b78c5a5e57494a3a9
Paulo Machado <paulomachado@outlook.com>
2015-12-09 10:10:32 -0300

    Resolvendo o problema da indústria grega

--- testes/teste_lu.py
+++ testes/teste_lu.py
@@ -18,11 +18,22 @@ from sist_linear.subst_suc import substituicoes_sucessivas
 from sist_linear.lu import lu
 
 def industria_grega():
-    ## escreva aqui o seu código
-    # n = ...
-    # A = ...
-    # alfa = ...
-    # beta = ...
+    n1 = 4
+    A1 = [[3, 2, 2,4],
+        [2, 3, 1, 2],
+        [4, 1, 3, 5],
+        [1, 4, 4, 3]]
+    b1 = [133,90,163,126]
+    b2 = [163,110,196,181]
+    
+    (L,U) = lu(n1,A1)
+    y = substituicoes_sucessivas(n1, L, b1)
+    x = substituicoes_retroativas(n1, U, y)
+    print("Na fábrica Alfa, serão produzidas %s %s %s e %s unidades dos produtos (1), (2), (3) e (4), respectivamente"%tuple(x))
+    
+    y = substituicoes_sucessivas(n1, L, b2)
+    x = substituicoes_retroativas(n1, U, y)
+    print("Na fábrica Alfa, serão produzidas %s, %s, %s e %s unidades dos produtos (1), (2), (3) e (4), respectivamente"%tuple(x))
 
 
 industria_grega()
