a325b8b842141f04e8bda753e3e88f1437cecca6
Paulo Machado <paulomachado@outlook.com>
2016-01-06 10:37:07 -0300

    Interpolação por Gauss

--- interpolacao/poli_gauss.py
+++ interpolacao/poli_gauss.py
@@ -20,7 +20,8 @@ def interpola_gauss(x, y, n):
     ## A primeira coisa a fazer é checar que o número de elementos em x e y é
     ## igual a n+1. Se forem diferentes, saia da função retornando None.
     
-    #if len(x) != n+1 or len(y)...
+    if len(x) != n+1 or len(y) != n+1:
+        return None
     
     ## Inicialize a matriz de Vandermonde, inicializando a matriz a partir de 
     ## uma matriz identidade.
@@ -29,30 +30,32 @@ def interpola_gauss(x, y, n):
     
     ## Você precisará de dois loops encadeados, um para percorrer as colunas e
     ## outro para percorrer os elementos de x (linhas) e popular a matriz A
-    
-    
+    for i in range(0,n+1):
+        for j in range(0,n+1):
+            A[i][j] = x[i]**j
     ## Chame o método de Gauss, lembrando que a ordem da matriz é n+1
-    #x = gauss(...)
+    x = gauss(n+1, A, y)
     
     ##retorne o vetor solução
-    
+    return x
 
 if __name__ == "__main__":
     ## Teste a função interpola_gauss para interpolação linear
     def teste_interp_linear():
-        #x = [...]
-        #y = [...]
-        #n = ...
-        #p = interpola_gauss(...)
-        #v = poli_aval(...)
-        #print(v)
+        x = [0.1, 0.6]
+        y = [1.221, 3.32]
+        n = 1
+        p = interpola_gauss(x,y,n)
+        v = poli_aval(p,0.2)
+        print(v)
        
     def teste_interp_quadratica():
-        #x = [...]
-        #y = [...]
-        #n = ...
-        #p = interpola_gauss(...)
-        #v = poli_aval(...)
-        #print(v) 
-    teste_interp_linear()
-    teste_interp_quadratica()
+        x = [-1, 0, 2]
+        y = [4, 1, -1]
+        n = 2 
+        p = interpola_gauss(x,y,n)
+        v = poli_aval(p, 1)
+        print(v) 
+        
+teste_interp_linear()
+teste_interp_quadratica()
