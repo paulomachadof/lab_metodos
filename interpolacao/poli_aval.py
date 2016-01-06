d0472826bc50bec3a0cc6aca8f4be382bcaa7017
Paulo Machado <paulomachado@outlook.com>
2016-01-06 09:13:49 -0300

    Função para avaliar polinômios

--- interpolacao/poli_aval.py
+++ interpolacao/poli_aval.py
@@ -7,10 +7,13 @@ def poli_aval(a, x):
     
     """
     n = len(a) - 1
-    
-    
+    b = a[n]
+    for i in range(n-1,0,-1):
+        b = b*x+a[i]
+    b = b*x+a[0]
+    return b
 if __name__ == "__main__":
-    #p = [...]
-    #x = ...
-    #v = poli_aval(...)
-    #print(v)
+    p = [3, 5, -3, 1]
+    x = 2
+    v = poli_aval(p,x)
+    print(v)
