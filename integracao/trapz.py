5b329151cfd3c3342247d9e486faa066d6320b87
Paulo Machado <paulomachado@outlook.com>
2016-01-13 10:04:06 -0300

    Regra do Trapézio Simples

--- integracao/trapz.py
+++ integracao/trapz.py
@@ -4,17 +4,23 @@ import sys
 sys.path.append("../")
 
 
-def trapz(x, y):
+def trapz(x, y, m=1):
     """
     Aproxima o valor da integral pela regra do trapézio, onde x é um vetor
     contendo x0 e x1 e y é um vetor contendo f(x0) e f(x1).
     Retorna o valor da integral.
     
     """
-    h = x[1] - x[0]
-    T = (h/2) * (y[0] + y[1])
+    if len(x) != m+1 or len(y) != m+1:
+        print("Erro!")
+        return None
     
-    return T
+    h = (x[m] - x[0]) / m
+    s = 0.0
+    for i in range(1, m):
+        s += y[i]
+    It= (2 * s + y[0] + y[m]) * h/2
+    return It
 
 if __name__ == "__main__":
     x = [1,7]
